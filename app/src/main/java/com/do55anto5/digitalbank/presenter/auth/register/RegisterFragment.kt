5fc161ac947720b82463bcd44f86fb510c81fba3
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-05-02 01:02:37 -0300

    FragmentRegister create

---
+++ app/src/main/java/com/do55anto5/digitalbank/presenter/auth/register/RegisterFragment.kt
@@ -0,0 +1,28 @@
+package com.do55anto5.digitalbank.presenter.auth.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.do55anto5.digitalbank.R
+import com.do55anto5.digitalbank.databinding.FragmentRecoverBinding
+import com.do55anto5.digitalbank.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
