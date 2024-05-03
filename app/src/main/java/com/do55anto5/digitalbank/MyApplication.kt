fff2f4a1f8df9646d8872184e2ef5f861049adb3
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-05-03 19:18:14 -0300

    create and configure class MyApplication

---
+++ app/src/main/java/com/do55anto5/digitalbank/MyApplication.kt
@@ -0,0 +1,12 @@
+package com.do55anto5.digitalbank
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/com/do55anto5/digitalbank/presenter/MainActivity.kt
+++ app/src/main/java/com/do55anto5/digitalbank/presenter/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.do55anto5.digitalbank
+package com.do55anto5.digitalbank.presenter
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.do55anto5.digitalbank.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
