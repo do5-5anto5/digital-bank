b8fe032db748bd270867433cb1e2ad45d1642af0
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-05-03 20:30:07 -0300

    configure Firebase instance

---
+++ app/src/main/java/com/do55anto5/digitalbank/dependency_injection/FirebaseModule.kt
@@ -0,0 +1,20 @@
+package com.do55anto5.digitalbank.dependency_injection
+
+import com.google.firebase.database.FirebaseDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+
+    @Singleton
+    @Provides
+    fun provideFirebaseDatabase() : FirebaseDatabase {
+        return FirebaseDatabase.getInstance()
+    }
+
+}
