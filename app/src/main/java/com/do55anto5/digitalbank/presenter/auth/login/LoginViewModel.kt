4592a7e5c97865272a6ca49b09de9b4dfd88dea3
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-05-05 09:35:37 -0300

    authentication feature - create LoginViewModel

---
+++ app/src/main/java/com/do55anto5/digitalbank/presenter/auth/login/LoginViewModel.kt
@@ -0,0 +1,29 @@
+package com.do55anto5.digitalbank.presenter.auth.login
+
+import androidx.lifecycle.liveData
+import com.do55anto5.digitalbank.data.model.User
+import com.do55anto5.digitalbank.domain.auth.LoginUsecase
+import com.do55anto5.digitalbank.util.StateView
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val loginUsecase: LoginUsecase
+) {
+
+    fun login(email: String, password: String) = liveData(Dispatchers.IO) {
+        try {
+            emit(StateView.Loading())
+
+            loginUsecase.invoke(email, password)
+
+            emit(StateView.Success(null))
+
+        } catch (e: Exception) {
+            emit(StateView.Error(e.message))
+        }
+    }
+
+}
