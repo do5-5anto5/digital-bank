98e1a834d7ebca30ad88fd4d23971e989587f729
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-05-05 10:09:59 -0300

    authentication feature - create RecoverViewModel

---
+++ app/src/main/java/com/do55anto5/digitalbank/presenter/auth/recover/RecoverViewModel.kt
@@ -0,0 +1,28 @@
+package com.do55anto5.digitalbank.presenter.auth.recover
+
+import androidx.lifecycle.liveData
+import com.do55anto5.digitalbank.domain.auth.RecoverUsecase
+import com.do55anto5.digitalbank.util.StateView
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+
+@HiltViewModel
+class RecoverViewModel @Inject constructor(
+    private val recoverUsecase: RecoverUsecase
+) {
+
+    fun recover(email: String) = liveData(Dispatchers.IO) {
+        try {
+            emit(StateView.Loading())
+
+            recoverUsecase.invoke(email)
+
+            emit(StateView.Success(null))
+
+        } catch (e: Exception) {
+            emit(StateView.Error(e.message))
+        }
+    }
+
+}
