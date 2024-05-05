fcdd760c593627e024f86fd381f0d25286ba51e0
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-05-05 19:14:02 -0300

    verify authentication in SplashFragment

--- app/src/main/java/com/do55anto5/digitalbank/presenter/splash/SplashFragment.kt
+++ app/src/main/java/com/do55anto5/digitalbank/presenter/splash/SplashFragment.kt
@@ -1,13 +1,19 @@
 package com.do55anto5.digitalbank.presenter.splash
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.do55anto5.digitalbank.R
 import com.do55anto5.digitalbank.databinding.FragmentSplashBinding
+import com.do55anto5.digitalbank.util.FireBaseHelper
 import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SplashFragment : Fragment() {
 
     private var _binding: FragmentSplashBinding? = null
@@ -19,6 +25,20 @@ class SplashFragment : Fragment() {
     ): View {
         _binding = FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed(this::verifyAuth, 3000)
+    }
+
+    private fun verifyAuth() {
+        if (FireBaseHelper.isAuthenticated()) {
+            findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+        } else {
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        }
     }
 
     override fun onDestroy() {
