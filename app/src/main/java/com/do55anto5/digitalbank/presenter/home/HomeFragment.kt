a078dbc2a386cb00ffab184e5bac039eaf28934c
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-05-09 18:25:21 -0300

    initial implementation of last transaction item; get mask for balance text on Home

--- app/src/main/java/com/do55anto5/digitalbank/presenter/home/HomeFragment.kt
+++ app/src/main/java/com/do55anto5/digitalbank/presenter/home/HomeFragment.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.viewModelScope
 import com.do55anto5.digitalbank.R
 import com.do55anto5.digitalbank.data.model.Wallet
 import com.do55anto5.digitalbank.databinding.FragmentHomeBinding
-import com.do55anto5.digitalbank.util.FireBaseHelper
 import com.do55anto5.digitalbank.util.GetMask
 import com.do55anto5.digitalbank.util.StateView
 import com.do55anto5.digitalbank.util.showBottomSheet
@@ -63,7 +60,7 @@ class HomeFragment : Fragment() {
 
     private fun showBalance(wallet: Wallet) {
         binding.txtBalance.text = getString(
-            R.string.currency_symbol, GetMask.getFormattedValue(wallet.balance))
+            R.string.home_fragment_currency_symbol, GetMask.getFormattedValue(wallet.balance))
     }
 
     override fun onDestroy() {
