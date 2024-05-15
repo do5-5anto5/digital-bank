2eeae296df72b937052ac34c23d477ebec552acc
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-05-15 16:00:27 -0300

    initRecyclerview in TransferUserListFragment

--- app/src/main/java/com/do55anto5/digitalbank/presenter/features/transfer/TransferUserListFragment.kt
+++ app/src/main/java/com/do55anto5/digitalbank/presenter/features/transfer/TransferUserListFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.do55anto5.digitalbank.databinding.FragmentTransferUserListBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class TransferUserListFragment : Fragment() {
 
     private var _binding: FragmentTransferUserListBinding? = null
@@ -23,8 +26,22 @@ class TransferUserListFragment : Fragment() {
         return binding.root
     }
 
-    private fun initListeners() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+
+        val adapterTransferUser = TransferUserAdapter { selectedUser ->
+            Toast.makeText(requireContext(), selectedUser.name, Toast.LENGTH_SHORT).show()
+        }
+
+        with(binding.rvUserList) {
+            setHasFixedSize(true)
+            adapter = adapterTransferUser
+        }
     }
 
     override fun onDestroyView() {
