850e1df5b93639d636134959667b51b560d217cb
do5-5anto5 <dos.santos.2k23@gmail.com>
2024-05-15 15:02:40 -0300

    implement GetProfilesListUseCase and TransferUserListViewModel

---
+++ app/src/main/java/com/do55anto5/digitalbank/presenter/transaction/TransferUserListViewModel.kt
@@ -0,0 +1,28 @@
+package com.do55anto5.digitalbank.presenter.transaction
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.do55anto5.digitalbank.domain.profile.GetProfilesListUseCase
+import com.do55anto5.digitalbank.util.StateView
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+
+@HiltViewModel
+class TransferUserListViewModel @Inject constructor(
+    private val getProfilesListUseCase: GetProfilesListUseCase
+) : ViewModel() {
+
+    fun getProfilesList() = liveData(Dispatchers.IO) {
+        try {
+            emit(StateView.Loading())
+
+            val usersList = getProfilesListUseCase.invoke()
+
+            emit(StateView.Success(usersList))
+
+        } catch (e: Exception) {
+            emit(StateView.Error(e.message))
+        }
+    }
+}
